295b9c7e3c5daa598ed2f14dc2fb13a07bbc478b
Faisal <nirzon192@hotmai.com>
2020-02-08 21:02:03 +0600

    Initial commit

---
+++ app/src/main/java/com/faisal/dc/activitylifecycle/MainActivity.java
@@ -0,0 +1,49 @@
+package com.faisal.dc.activitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG="MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e(TAG,"1> In on Create method");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG,"2> In on Start method");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG,"3> In on Resume method");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG,"4> In on Pause method");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG,"5> In on Stop method");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.e(TAG,"6> In on Destroy method");
+    }
+}
