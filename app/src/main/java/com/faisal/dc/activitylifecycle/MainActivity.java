ea5aa9edbb16c8a612ce2dfa912e3255ca334299
Faisal <nirzon192@hotmai.com>
2020-02-08 21:19:10 +0600

    add commet to method

--- app/src/main/java/com/faisal/dc/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/faisal/dc/activitylifecycle/MainActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG="MainActivity";
+
+    // on create
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,31 +17,35 @@ public class MainActivity extends AppCompatActivity {
         Log.e(TAG,"1> In on Create method");
     }
 
+    // onStart
     @Override
     protected void onStart() {
         super.onStart();
         Log.e(TAG,"2> In on Start method");
     }
 
-
+    // onResume
     @Override
     protected void onResume() {
         super.onResume();
         Log.e(TAG,"3> In on Resume method");
     }
 
+    // onPause
     @Override
     protected void onPause() {
         super.onPause();
         Log.e(TAG,"4> In on Pause method");
     }
 
+    // onStop
     @Override
     protected void onStop() {
         super.onStop();
         Log.e(TAG,"5> In on Stop method");
     }
 
+    // onDestroy
     @Override
     protected void onDestroy() {
         super.onDestroy();
